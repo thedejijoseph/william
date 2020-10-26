cdf56e0a78794735d4e7cf95b26a4b227062e95d
Deji <wrecodde@gmail.com>
2020-10-26 15:07:49 +0100

    quill-notes basics is up
    tested basic text editing and display for quilljs
    need to work on images and any other fancy stuff.. after testing
    tinymce and editor.js though
    
    and in my search did i come up on opinion that markdown might just
    be the best alternative, given that we're not doing anything fancy

--- quill/models.py
+++ quill/models.py
@@ -1,3 +1,10 @@
+from typing import Match
 from django.db import models
 
-# Create your models here.
+class QuillNote(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.EmailField(default='admin@quill-notes.com')
+    title = models.CharField(max_length=500)
+    quill_delta = models.TextField(default='', blank=True)
+    raw_text = models.TextField(default='', blank=True)
+    note_id = models.UUIDField(blank=False)
