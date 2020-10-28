f49f85dcb2604f6f725284141b4dff8249f0029c
Deji <wrecodde@gmail.com>
2020-10-28 23:11:38 +0100

    prepped for heroku
    updated Procfile
    moved database to postgres
    tracked migrations/ dir

--- william/settings.py
+++ william/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'william.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PG_DB_NAME'),
+        'USER': os.getenv('PG_DB_USER'),
+        'HOST': os.getenv('PG_DB_HOST'),
+        'PORT': os.getenv('PG_DB_PORT'),
+        'PASSWORD': os.getenv('PG_DB_PASSWORD')
     }
 }
 
